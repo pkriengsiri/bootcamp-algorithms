d60edf050f26171d7d548d2086f8279743ec5d2f
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-03-13 21:34:55 -0500

    Refactoed #30

--- 30-multiply-into-20/Unsolved/multiply-into-20.js
+++ 30-multiply-into-20/Unsolved/multiply-into-20.js
@@ -1,38 +1,26 @@
 // Write code to create a function that accepts an array of unique numbers
 // If any two numbers in the array add up to 20, return true, else return false
 
-var multiplyInto20 = function(arr) {
-    if(arr.includes(-20)) {
-        if(arr.includes(-1)) {
+
+
+const multiplyInto20 = (arr) => {
+    // Map to store numbers in the array
+    const arrMap = new Map();
+
+    // iterate over the array
+    for (let i = 0; i < arr.length; i++) {
+        // Find the number needed to be divisible by 20
+        let numberNeeded = 20 / arr[i];
+        // Check if the map has this number as a key
+        if (arrMap.has(numberNeeded)) {
+            // return true if it does
             return true;
+        } else {
+            // Add the number to the map if it doesn't
+            arrMap.set(arr[i], 1);
         }
     }
-    if(arr.includes(-10)) {
-        if(arr.includes(-2)) {
-            return true;
-        }
-    }
-    if(arr.includes(-5)) {
-        if(arr.includes(-4)) {
-            return true;
-        }
-    }
-    if(arr.includes(20)) {
-        if(arr.includes(1)) {
-            return true;
-        }
-    }
-    if(arr.includes(10)) {
-        if(arr.includes(2)) {
-            return true;
-        }
-    }
-    if(arr.includes(5)) {
-        if(arr.includes(4)) {
-            return true;
-        }
-    }
-    
+    // Return false if no matches found
     return false;
-    
-};
+
+}
