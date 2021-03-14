c19a6b5e17397001e302326e8871a428578bf35d
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-03-13 19:54:47 -0500

    Solved #27

--- 27-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
+++ 27-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
@@ -3,5 +3,21 @@
 // Return the maximum number of songs from the list that can fit in a 60 minute playlist
 
 var mostSongsInPlaylist = function(arr) {
-  // add your code here...
+  // sort the array
+  arr.sort((a,b)=> a-b);
+  let songCount = 0;
+  let totalMinutes = 0
+  // iterate over the array
+  for(let i =0; i <= arr.length; i++) {
+    // add the current song's length to the total minutes array
+    totalMinutes += arr[i];
+    // check to see if total minutes is more than 60, or if we are at the end of the array
+    if(totalMinutes >60 || i === arr.length) {
+      // return the count
+      return songCount;
+    } else {
+      // add one to the count
+      songCount++;
+    }
+  }
 };
