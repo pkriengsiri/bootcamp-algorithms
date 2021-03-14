23648ffa0670b4ea7aac7242ebff2130d93d0473
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-03-13 21:19:33 -0500

    Solved #30

--- 30-multiply-into-20/Unsolved/multiply-into-20.js
+++ 30-multiply-into-20/Unsolved/multiply-into-20.js
@@ -1,4 +1,38 @@
 // Write code to create a function that accepts an array of unique numbers
 // If any two numbers in the array add up to 20, return true, else return false
 
-var multiplyInto20 = function(arr) {};
+var multiplyInto20 = function(arr) {
+    if(arr.includes(-20)) {
+        if(arr.includes(-1)) {
+            return true;
+        }
+    }
+    if(arr.includes(-10)) {
+        if(arr.includes(-2)) {
+            return true;
+        }
+    }
+    if(arr.includes(-5)) {
+        if(arr.includes(-4)) {
+            return true;
+        }
+    }
+    if(arr.includes(20)) {
+        if(arr.includes(1)) {
+            return true;
+        }
+    }
+    if(arr.includes(10)) {
+        if(arr.includes(2)) {
+            return true;
+        }
+    }
+    if(arr.includes(5)) {
+        if(arr.includes(4)) {
+            return true;
+        }
+    }
+    
+    return false;
+    
+};
