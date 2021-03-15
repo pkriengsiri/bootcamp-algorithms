a5a7f6b04eaec26d30356d2f1c3cb4050bd847e7
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-03-15 10:13:23 -0400

    Solved #34

--- 34-permutation-substring/Unsolved/permutation-substring.js
+++ 34-permutation-substring/Unsolved/permutation-substring.js
@@ -3,9 +3,13 @@
 // Else return false
 
 var permutationSubstring = function(str, sub) {
+    // split the strings so that array methods can be used
     const strArray = str.split("");
     const subArray = sub.split("");
+    // create a map to hold letters from the string
     const strMap = {};
+    // iterate over the string and add letters to the map
     strArray.forEach(letter => strMap[letter] = true);
+    // evaluated whether of every letter of the substring exists in the map
     return subArray.every(letter => strMap[letter]);
 };
