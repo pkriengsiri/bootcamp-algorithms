94cdaf1eaf451224abb663db1ba9c8afb95d8595
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-03-16 19:07:39 -0400

    Solved #39

--- 39-square-root (solved)/Unsolved/square-root.js
+++ 39-square-root (solved)/Unsolved/square-root.js
@@ -1,4 +1,14 @@
 // Write code to create a function that accepts a non-negative integer and returns the square root of the integer. If the square root is a decimal number, round down to the nearest whole integer
 // You may not use the built-in `Math.sqrt` method
 
-var sqrt = function(x) {};
+var sqrt = function(x) {
+    for(let i = 0; i <= x; i++) {
+        if(i*i > x) {
+            return i-1;
+        } else {
+            if(i*i === x) {
+                return i;
+            }
+        }
+    }
+};
