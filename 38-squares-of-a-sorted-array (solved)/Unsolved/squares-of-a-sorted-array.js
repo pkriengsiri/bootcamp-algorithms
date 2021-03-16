13bcec50c686e4da14e3337f23d1ada96e28f901
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-03-16 18:59:51 -0400

    Solved #38

--- 38-squares-of-a-sorted-array (solved)/Unsolved/squares-of-a-sorted-array.js
+++ 38-squares-of-a-sorted-array (solved)/Unsolved/squares-of-a-sorted-array.js
@@ -1,3 +1,6 @@
 // Write code to create a function that accepts an array of integers sorted in ascending (increasing) order and returns a new array containing the squares of each number in ascending order
 
-var sortedSquares = function(arr) {};
+var sortedSquares = function(arr) {
+    // map the array with each element squared, then sort the mapped array
+    return arr.map(number => number*number).sort((a,b) => a-b);
+};
