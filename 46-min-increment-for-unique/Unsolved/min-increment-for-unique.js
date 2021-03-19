0a399bb3f098ce5d4d5769c22aae05a5cb77f828
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-03-19 18:20:24 -0400

    Solved #46

--- 46-min-increment-for-unique/Unsolved/min-increment-for-unique.js
+++ 46-min-increment-for-unique/Unsolved/min-increment-for-unique.js
@@ -1,3 +1,35 @@
 // Write a function to take an array of integers and return the minimum number of increments required across the array to ensure that every number is unique
 
-var minIncrement = function(nums) {};
+var minIncrement = function(nums) {
+    // array to hold unique values
+    const uniqueArr = [];
+
+    // array to hold duplicate values
+    const duplicateArr = [];
+
+    // variable to hold number of increments
+    let increments = 0;
+
+    // interate over the array of numbers and separate unique values from duplicate values
+    nums.forEach(number => {
+        if(uniqueArr.includes(number)) {
+            duplicateArr.push(number);
+        } else {
+            uniqueArr.push(number);
+        }
+    });
+
+    // iterate over the array of duplicate values and increment each value until it is unique
+    duplicateArr.forEach(number => {
+        let currentNumber = number;
+        while(uniqueArr.includes(currentNumber)) {
+            currentNumber++;
+            increments++;
+        };
+        // push the new unique number to the array
+        uniqueArr.push(currentNumber);
+    });
+    
+    return increments;
+
+};
